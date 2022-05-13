4cb7c273d5e0c1fca2dc03fd228c52bc7be39449
Ferminpg <ferminpozo2k@gmail.com>
2022-05-13 13:34:04 +0200

    Solucionado conflicto preuba_01_126.sh rama funciones

--- prueba_01_126.sh
+++ prueba_01_126.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 clear
-. funciones/funciones.sh
-saludo
+#<<<<<<<<<<<HEAD
 echo "Soy el script 01"
 #Main: Voy a crear un conflicto entre esta rama y la otra.
-echo "Soy el script 01 con un conflicto en la misma linea"
+#echo "Soy el script 01 con un conflicto en la misma linea"
+#=========
+. funciones/funciones.sh
+saludo
+#echo "Soy el script 01"
+#>>>>>>>>>>funciones
 exit 0
